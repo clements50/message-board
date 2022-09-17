16cfecd90fb0d6615aa2c40d0db47db3b9ca7cd6
Cody Clements <subliminalverses2@gmail.com>
2022-09-17 01:59:43 -0500

    create index view template style it and add template messages

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const path = require("path");
 const indexRouter = require("./routes/index");
 
 const app = express();
 
 app.set("view engine", "ejs");
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/", indexRouter);
 
 const port = 3000;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+const messages = [
+  {
+    user: "Sauske",
+    message: "It's not the future I dream of anymore, only the past.",
+    date: new Date(),
+  },
+  {
+    user: "Jiraiya",
+    message:
+      "When peopel get hurt, they learn to hate... When people hurt others, they become hated and racked with guilt. But Knowing that pain allows people to be kind. Pain allows people to grow... And how you grow is up to you.",
+    date: new Date(),
+  },
+  {
+    user: "Naruto",
+    message:
+      "It's not face that makes someone a monster; Its the choices they make with their lives.",
+    date: new Date(),
+  },
+];
+
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { messages: messages });
 });
 
 module.exports = router;
